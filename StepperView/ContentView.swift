f5ce7a90d84f7d244879995454722aa5b930787d
Waihon Yew <yewwaihon@gmail.com>
2021-05-26 11:16:26 +0800

    Add a Stepper with value bound to sleepAmount

--- StepperView/ContentView.swift
+++ StepperView/ContentView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var sleepAmount = 8.0
+  
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    Stepper(value: $sleepAmount) {
+      Text("\(sleepAmount) hours")
+    }
   }
 }
 
