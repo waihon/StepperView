9fcb6ed574cfc9ddc1073a674e91fc17871dad45
Waihon Yew <yewwaihon@gmail.com>
2021-05-26 11:09:39 +0800

    Initial Commit

---
+++ StepperView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StepperView
+//
+//  Created by Waihon Yew on 26/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
